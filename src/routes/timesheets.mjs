b380920cf073babf51f57fa5832afd7392d56459
Bipin Shrestha <bipins@marketamerica.com>
2025-10-16 16:05:30 +0545

    Add all CRUD to timesheet

--- src/routes/timesheets.mjs
+++ src/routes/timesheets.mjs
@@ -6,12 +6,63 @@ import ExcelJs from "exceljs";
 
 const router = Router();
 
+router.get("/api/timesheet/:day/:month/:year", async (req, res) => {
+  const {
+    params: { day, month, year },
+  } = req;
+  const startDate = new Date(`${year}-${month}-${day}`);
+  const endDate = new Date(startDate);
+  endDate.setDate(startDate.getDate() + 1);
+
+  try {
+    const result = await Timesheet.find({
+      date: { $gte: startDate, $lte: endDate },
+    });
+    if (result.length > 0) {
+      console.log("res", result[0]);
+
+      return res.send(result).status(200);
+    } else {
+      return res.send("No timesheet found!").status(404);
+    }
+  } catch (err) {
+    return res.send("Something went wrong!").status(404);
+  }
+});
+
 router.get("/api/timesheet/:month/:year", async (req, res) => {
+  console.log("also reaches here");
   const {
     params: { month, year },
   } = req;
 
-  console.log("Getting all timesheet", month, year);
+  const formattedMonth = month.padStart(2, "0");
+
+  const startDate = new Date(`${year}-${formattedMonth}-01T00:00:00.000Z`);
+  const endDate = new Date(
+    new Date(startDate).setMonth(startDate.getMonth() + 1) - 1
+  );
+  try {
+    const result = await Timesheet.find({
+      date: { $gte: startDate, $lte: endDate },
+    });
+    if (result.length > 0) {
+      return res.send(result).status(200);
+    } else {
+      return res.send("No timesheet found for the given month").status(404);
+    }
+
+    // res.send(result).status(200);
+  } catch (err) {
+    return res.send("Something went wrong!").status(400);
+  }
+});
+
+router.get("/api/timesheet-export/:month/:year", async (req, res) => {
+  const {
+    params: { month, year },
+  } = req;
+
   const startDate = new Date(`${year}-${month}-01T00:00:00.000Z`);
   const endDate = new Date(
     new Date(startDate).setMonth(startDate.getMonth() + 1) - 1
@@ -21,7 +72,6 @@ router.get("/api/timesheet/:month/:year", async (req, res) => {
       date: { $gte: startDate, $lte: endDate },
     });
     if (result.length > 0) {
-      console.log("processing xls");
       const workbook = new ExcelJs.Workbook();
       const worksheet = workbook.addWorksheet("Timesheet");
       worksheet.columns = [
@@ -51,14 +101,12 @@ router.get("/api/timesheet/:month/:year", async (req, res) => {
         "Content-Disposition",
         "attachment; filename=timesheets.xlsx"
       );
-      res.send(buffer);
+      return res.send(buffer);
     } else {
-      res.sendStatus(200);
+      return res.sendStatus(200);
     }
-
-    // res.send(result).status(200);
   } catch (err) {
-    console.log("error", err);
+    return res.send("Something went wrong!").status(400);
   }
 });
 
@@ -72,7 +120,6 @@ router.post(
     }
     const data = matchedData(req);
 
-    console.log("data", data);
     const newTimesheet = new Timesheet(data);
     try {
       const savedTimesheet = await newTimesheet.save();
@@ -82,5 +129,31 @@ router.post(
     }
   }
 );
+
+router.delete("/api/timesheet/:day/:month/:year", async (req, res) => {
+  const {
+    params: { day, month, year },
+  } = req;
+
+  const startDate = new Date(`${year}-${month}-${day}`);
+  const endDate = new Date(startDate);
+  endDate.setDate(startDate.getDate() + 1);
+  try {
+    const result = await Timesheet.deleteOne({
+      date: {
+        $gte: startDate,
+        $lte: endDate,
+      },
+    });
+
+    if (result.deletedCount > 0) {
+      return res.send("Timesheet deleted").status(200);
+    } else {
+      return res.send("Couldnt find timesheet!").status(404);
+    }
+  } catch (err) {
+    return res.send("Something went wrong!").status(400);
+  }
+});
 
 export default router;
